08cb663c21bbb89c0dfeb97671a98eba4d0c60e3
khainqd <khainqd00575@fpt.edu.vn>
2018-12-12 21:52:49 +0700

    Tạo area và giao diện login

---
+++ Model/Dao/UserDao.cs
@@ -0,0 +1,37 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    public class UserDao
+    {
+        OnlineShopDbContext db = null;
+        public UserDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+        public long Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        public bool Login(string userName, string passWord)
+        {
+            var result = db.Users.Count(x => x.UserName == userName && x.Password == passWord);
+            if(result > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
